e885d619c1bbe6bf397b1e63062ee053b71b2fed
Pritam Prasad <5097255+pritamprasd@users.noreply.github.com>
2026-07-08 21:22:26 +0530

    migration fix

--- backend/migrations/versions/b8c9d0e1f2a3_add_is_hidden_to_imported_files.py
+++ backend/migrations/versions/b8c9d0e1f2a3_add_is_hidden_to_imported_files.py
@@ -1,7 +1,7 @@
 """add is_hidden column to imported_files
 
 Revision ID: b8c9d0e1f2a3
-Revises: a7b8c9d0e1f2
+Revises: g7h8i9j0k1l2
 Create Date: 2026-07-08 12:00:00.000000
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 revision = 'b8c9d0e1f2a3'
-down_revision = 'a7b8c9d0e1f2'
+down_revision = 'g7h8i9j0k1l2'
 branch_labels = None
 depends_on = None
 
